e0842e93d31db577ac68d13257eddf8583c9507f
Kamran Hajili <khajili98@gmail.com>
2021-08-02 15:47:20 +0400

    second version is worked

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,122 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server.dart';
+import 'package:mailer/smtp_server/gmail.dart';
 
 void main() {
   runApp(MyApp());
 }
+//second version
+
+sendEmail() async {
+  final Email email = Email(
+    body: 'Email body',
+    subject: 'Email subject',
+    recipients: ['shaban.haciyev@gmail.com'],
+    isHTML: false,
+  );
+
+  await FlutterEmailSender.send(email);
+}
+
+//first version
+// sendMail() async {
+//   String username = 'khajili98@gmail.com';
+//   String password = '3003880Kam';
+
+//   // Create our message.
+//   final message = Message()
+//     ..from = Address(username, 'Kamran')
+//     ..recipients.add('shaban.haciyev@gmail.com')
+//     ..subject = 'Test Dart Mailer library :: 😀 :: ${DateTime.now()}'
+//     ..text = 'This is the plain text.\nThis is line 2 of the text part.'
+//     ..html = "<h1>Test</h1>\n<p>Hey! Here's some HTML content</p>";
+
+//   SmtpServer? smtpServer;
+
+//   try {
+//     // Setting up Google SignIn
+//     final googleSignIn = GoogleSignIn.standard(scopes: [
+//       'khajili98@gmail.com',
+//       'https://www.googleapis.com/auth/gmail.send'
+//     ]);
+
+//     // Signing in
+//     final account = await googleSignIn.signIn();
+
+//     if (account == null) {
+//       // User didn't authorize
+//       return;
+//     }
+
+//     final auth = await account.authentication;
+
+//     // Creating SMTP server from the access token
+//     smtpServer = gmailSaslXoauth2(username, auth.accessToken!);
+//   } on Exception catch (e) {
+//     // ignore: todo
+//     // TODO: Handle auth error
+//     print(e);
+//   }
+
+// // Ready to send a message now
+
+//   // Use the SmtpServer class to configure an SMTP server:
+//   // final smtpServer = SmtpServer('smtp.domain.com');
+//   // See the named arguments of SmtpServer for further configuration
+//   // options.
+
+//   try {
+//     final sendReport = await send(message, smtpServer!);
+//     print('Message sent: ' + sendReport.toString());
+//   } on MailerException catch (e) {
+//     print('Message not sent.');
+//     for (var p in e.problems) {
+//       print('Problem: ${p.code}: ${p.msg}');
+//     }
+//   }
+
+//   var connection = PersistentConnection(smtpServer!);
+
+//   // Send the first message
+//   await connection.send(message);
+
+//   // send the equivalent message
+
+//   // close the connection
+//   await connection.close();
+// }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 200,
+                ),
+                Text("salam"),
+                TextButton(
+                    onPressed: () {
+                      print("salam");
+                      sendEmail();
+                    },
+                    child: Text("Send"))
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
